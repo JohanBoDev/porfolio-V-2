aec397618295e48239b459e514d34a35d4ceb332
JohanBoDev <bohorquezjohan958@gmail.com>
2024-04-08 01:26:38 -0500

    Version-2

---
+++ Portafolio-Version-1-master/tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./src/**/*.{html,js}"],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+    fontFamily: {
+      'sans': ['ui-sans-serif', 'system-ui'],
+      'serif': ['ui-serif', 'Georgia'],
+      'mono': ['ui-monospace', 'SFMono-Regular'],
+      'display': ['Oswald'],
+      'body': ['Open Sans'],
+      'heading': ['font-Press Start 2P'],
+     }
+  }
